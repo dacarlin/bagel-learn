1d266daadf41b9ff06f074a9f8eb21316a3641e0
Alex Carlin <carlin@ucdavis.edu>
2015-12-02 20:28:03 -0800

    Clean

--- benchmark/rosetta_input_files/command_line.bash
+++ benchmark/rosetta_input_files/command_line.bash
@@ -1,4 +1,14 @@
-rosetta_scripts.linuxgccrelease @ flags 
+#$ -S /bin/bash 
+#$ -cwd 
+#$ -o logs
+#$ -e logs 
+#$ -N benchmark 
+
+LINE=$( sed -n ${SGE_TASK_ID}p list.txt )
+S=$( echo $LINE | cut -d' ' -f1 )
+SUF=$( echo $LINE | cut -d' ' -f2 )
+
+~/rosetta/source/bin/rosetta_scripts.linuxgccrelease @ flags -s $S -suffix=$SUF
 
 # need -s to run (flags calls for nstruct=1)
 # list should look like 
